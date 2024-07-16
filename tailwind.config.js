859d2dc65a09645bf96e20f0871b556006c14c80
EduxxT <93246275+EduxxT@users.noreply.github.com>
2024-07-16 15:26:07 -0600

    2do commit (Working views)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["../www/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {}
   },
   plugins: [],
 }
-
--- www/js/partialview.js
+++ www/js/partialview.js
@@ -1,17 +1,22 @@
 document.addEventListener('deviceready', onDeviceReady, false);
 
-function loadPartialView(viewName, divClass = null, isAppend = false){
+function loadPartialView(viewName, divClass = null, isAppend = false){    
     $.ajax({
-        url: 'Views/' + viewName + '.html',
+        url: 'views/' + viewName + '.html',
         method: 'GET',
         success: function(data){
-            divClass === null ? console.error('Elemento contenedor (divClass) no definido') : (isAppend ? $(divClass).append(data) : $(divClass).html(data));
-        },
+            if (divClass === null) {
+                console.error('Elemento contenedor (divClass) no definido');
+                return;
+            }
+            isAppend ? $(divClass).append(data) : $(divClass).html(data);
+                    },
         error: function(xhr, status, error) {
             console.error('Error al cargar la vista parcial:', error);
         }
     })
 }
+
 
 function onDeviceReady() {
     // Cordova is now initialized. Have fun!
